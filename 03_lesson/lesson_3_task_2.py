2819afeb9d1fc4a8e03dac2d9015a2919525a6db
RiazanchikKet <katyunchik46@gmail.com>
2025-04-17 00:32:19 +0300

    Edit_lesson_3_task_2

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -3,7 +3,9 @@ from smartphone import Smartphone
 catalog = [
     Smartphone("Samsung", "Galaxy 10", "+7 900 800 32-32"),
     Smartphone("Xiaomi", "Not Pro 8", "+ 7 900 100 40-30"),
-    Smartphone("iPhone", "16 Pro", "+7 900 300 90-90")
+    Smartphone("iPhone", "16 Pro", "+7 900 300 90-90"),
+    Smartphone("Motorolla", "RAZR", "+7 900 400 90-80"),
+    Smartphone("Huawei", "pura 70 Pro", "+7 900 500 70-60")
 ]
 
 for smart in catalog:
